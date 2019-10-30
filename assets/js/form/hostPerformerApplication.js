9edea019683938583fb7ad7947a57e8856982bb1
Jason Ho <jho70@myseneca.ca>
2019-10-30 03:54:41 -0400

    finish host/performer form

--- assets/js/form/hostPerformerApplication.js
+++ assets/js/form/hostPerformerApplication.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzCNvndziqSX2A8qkFpUakEbk3VgGpjM7RyrFQiTQSEmMssUqJU/exec';
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw3938jM6S4ZkYqMZmkEO7Y6BKPR57ON6YEuWIgX9QgDFWCtprt/exec';
 const form = document.forms['submit-to-google-sheet'];
 
 var $inputs = $("input[name='phone number'], input[name='email']");
@@ -9,7 +9,7 @@ $inputs.on('input', function() {
 var $inputRadio = $('input[type="radio"][name="host/performer"]');
 $inputRadio.click(function() {
     var related_class = $(this).val();
-    $('.' + related_class).prop('disabled', false);
+    $('.' + related_class).prop('disabled', false);  
 
     $inputRadio.not(':checked').each(function() {
         var other_class = $(this).val();
@@ -17,9 +17,29 @@ $inputRadio.click(function() {
     })
 });
 
+$('#resume').on('change', function() {
+
+    var file = this.files[0];
+    var fr = new FileReader();
+    fr.fileName = file.name;
+    fr.onload = function(e) {
+      e.target.result;
+      html = '<input type="hidden" name="data" value="' + e.target.result.replace(/^.*,/, '') + '" >';
+      html += '<input type="hidden" name="mimetype" value="' + e.target.result.match(/^.*(?=;)/)[0] + '" >';
+      html += '<input type="hidden" name="filename" value="' + e.target.fileName + '" >';
+      $("#data").empty().append(html);
+    }
+    fr.readAsDataURL(file);
+  });
+
 form.addEventListener('submit', e => {
-    /*e.preventDefault();
-    fetch(scriptURL, {method: 'POST', mode: 'no-cors', body: new FormData(form)})
+    var data = new FormData(form);
+    if(document.getElementById("resume").files.length != 0){
+        data.append('resume', document.getElementById('resume').files);
+    }
+
+    e.preventDefault();
+    fetch(scriptURL, {method: 'POST', mode: 'no-cors', body: data})
         .then(res => {
             if(res.status < 400){
                 return;
@@ -27,16 +47,20 @@ form.addEventListener('submit', e => {
                 throw new Error('Fail');
             }
         })
-        .then(() => {*/
+        .then(() => {
             $("#success-message").attr("class", "alert alert-success alert-dismissible");
-            document.getElementById("message").innerText = "host performer application submitted successfully";
+            document.getElementById("message").innerText = "Application submitted successfully";
+            $('#data').empty();
+            document.getElementById("resume").value = "";
+            $('#resume').prop("disabled", true);
+            $('#performance').prop("disabled", true);
             document.getElementById("submit-to-google-sheet").reset();
             $('.alert').show();
-        /*}).catch(() => {
+        }).catch(() => {
             $("#success-message").attr("class", "alert alert-danger alert-dismissible");
             document.getElementById("message").innerText = "Error!";
             $('.alert').show();
-        });*/
+        });
 }); 
 
 function hideMessage() {
